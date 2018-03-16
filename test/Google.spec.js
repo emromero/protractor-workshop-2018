c6d2734b7e99f92af2c76d8e2234b0d341af0fa6
emromero <37183902+emromero@users.noreply.github.com>
2018-03-16 09:44:51 -0500

    improve-test (#2)
    Mejorando el primer caso de prueba

---
+++ test/Google.spec.js
@@ -0,0 +1,11 @@
+describe('This is thefirst example of protractor', () => {
+    describe('when open Google Page', () => {
+        beforeEach(() => {
+            browser.get('http://www.google.com');
+        });
+
+    it('then should have a title', () => {
+        expect(browser.getTitle()).toEqual('Google');
+    });
+ });
+});
