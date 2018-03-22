5119f18934726509815ad648b5f95284da0b0ae9
Erika Romero <emromerog@hotmail.com>
2018-03-22 11:19:27 -0500

    CSS Selector
    Se hace la parte del taller correspondiente a CSS Selector

--- protractor/headless.config.ts
+++ protractor/headless.config.ts
@@ -2,18 +2,19 @@ import​ { browser, Config } from​ 'protractor';
 import​ { reporter } from​ './helpers/reporter';
 
 export​ const​ config: Config = {
- framework: 'jasmine',
- SELENIUM_PROMISE_MANAGER: false,
- specs: ['../test/Google.spec.js'],
- noGlobals: true​,
- capabilities: {
+  framework: 'jasmine',
+  SELENIUM_PROMISE_MANAGER: false,
+  specs: ['../test/**/*.spec.js'],
+  getPageTimeout: 1000,
+  noGlobals: true​,
+  capabilities: {
     browserName: 'chrome',
     chromeOptions: {
-    args: ['--headless', '--disable-gpu', '--window-size=800,600']
+      args: ['--headless', '--disable-gpu', '--window-size=800,600']
     }
-},   
- onPrepare: () => {
- browser.ignoreSynchronization = true​;
- reporter();
- }
-}
+  },   
+  onPrepare: () => {
+    browser.ignoreSynchronization = true​;
+    reporter();
+  }
+};
