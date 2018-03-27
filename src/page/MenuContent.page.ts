2079b69befdf99c0569e7c9a30a96ed090f43514
Erika Romero <emromerog@hotmail.com>
2018-03-27 18:08:51 -0500

    Esperas Explicitas
    Esperas Explicitas

--- src/page/MenuContent.page.ts
+++ src/page/MenuContent.page.ts
@@ -1,11 +1,16 @@
-import​ { $, ElementFinder, promise } from​ 'protractor';
+import​ { $, ElementFinder, promise, ExpectedConditions, browser} from​ 'protractor';
 
 export​ class​ MenuContentPage {
+
+private EC = ExpectedConditions;//Declarar espera explicita
+
 private​ get​ tShirtMenu(): ElementFinder {
  return​ $('#block_top_menu > ul > li:nth-child(3) > a');
  }
  
 public​ goToTShirtMenu(): promise.Promise<void> {
- return​ this​.tShirtMenu.click();
+    let isClickable = this.EC.elementToBeClickable(this.tShirtMenu);//Esperar hasta que el elemento este disponible para ser clickeado
+    browser.wait(isClickable, 5000); //wait for condition isClickable to be true. y dar tiempo de espera de 5 segundos
+    return​ this​.tShirtMenu.click(); //wait for condition isClickable to be true.
  }
 }
